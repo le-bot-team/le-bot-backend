dcd90ecf26e7631636e1958dacb3101106356552
ParticleG <particle_g@outlook.com>
2025-07-17 04:31:39 +0800

    - Update sources

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import { swagger } from '@elysiajs/swagger'
 import { Elysia } from 'elysia'
 
-import { log } from '@log'
-
 import { authRoute } from '@auth/route'
-import { chatRoute } from './chat/route'
+import { chatRoute } from '@chat/route'
+import { log } from '@log'
 
 const app = new Elysia()
   .use(log.into())
   .use(swagger())
   .onError((ctx) => {
-    ctx.log?.error(ctx, ctx.error.toString());
-    return "onError";
+    ctx.log?.error(ctx, ctx.error.toString())
+    return 'onError'
   })
   .use(authRoute)
   .use(chatRoute)
   .listen(3000)
 
-log.info(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-)
+console.info('')
+log.info(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
